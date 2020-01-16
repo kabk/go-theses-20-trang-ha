ebb76d9542c32ef4caea240968ce6893e8da3d83
tranghasgit <tranghasgit@users.noreply.github.com>
2020-01-17 00:51:02 +0100

    initWEBmob

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+
+$(document).ready(function(){
+  $('.nav3').click(function(){
+    $('.commoItem').toggleClass('commoItemClicked');
+  });
+  $('.nav4').click(function(){
+    $('.layerItem').toggleClass('layerItemClicked');
+  });
+  $('.nav5').click(function(){
+    $('.explodeItem').toggleClass('explodeItemClicked');
+  });
+  $('.nav6').click(function(){
+    $('.alterItem').toggleClass('alterItemClicked');
+  });
+  $('.nav7').click(function(){
+    $('.communiItem').toggleClass('communiItemClicked');
+  });
+});
