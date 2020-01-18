0e3ea212dbf95dd7075b07e5a85b753e267b5f9a
tranghasgit <tranghasgit@users.noreply.github.com>
2020-01-18 13:47:38 +0100

    new drag element

--- js/main.js
+++ js/main.js
@@ -143,6 +143,59 @@ $(document).ready(function(){
     });
   });
 
+//Credits to Laurel Schwulst/beautiful-company.com on her Flight Simulator website
+//Flight Simulator is a collaboration between Laurel Schwulst and Soft.
+
+  // draggable window shade
+     dragElement(document.getElementById("window-shade"));
+     dragElement(document.getElementById("window-shade2"));
+
+     function dragElement(elmnt) {
+       var x_1 = 0,
+         x_2 = 0;
+       elmnt.addEventListener("touchstart", dragMouseDown);
+       elmnt.addEventListener("mousedown", dragMouseDown);
+
+       function dragMouseDown(e) {
+         e = e || window.event;
+         e.preventDefault();
+         // get event's x coordinate
+         x_2 = e.type === "touchmove" ? e.touches[0].clientX : e.clientX;
+
+         document.addEventListener("touchend", closeDragElement);
+         document.addEventListener("mouseup", closeDragElement);
+
+         document.addEventListener("touchmove", elementDrag);
+         document.addEventListener("mousemove", elementDrag);
+       }
+
+       function elementDrag(e) {
+         e = e || window.event;
+         // e.preventDefault();
+
+         // get event's x coordinate
+         var clientX = e.type === "touchmove" ? e.touches[0].clientX : e.clientX;
+
+         // calculate the new cursor position and update
+         x_1 = x_2 - clientX;
+         x_2 = clientX;
+
+         // set the element's new position. max: 0%, min: -85%.
+         elmnt.style.left =
+           Math.max(
+             0,
+             Math.min(85, ((elmnt.offsetLeft - x_1) / elmnt.offsetWidth) * 100)
+           ) + "%";
+       }
+
+       // stop moving when mouse button is released
+       function closeDragElement() {
+         document.removeEventListener("touchend", closeDragElement);
+         document.removeEventListener("mouseup", closeDragElement);
+         document.removeEventListener("touchmove", elementDrag);
+         document.removeEventListener("mousemove", elementDrag);
+       }
+     }
 
 
 });
