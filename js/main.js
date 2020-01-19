b5c35725bc46f64b8999a6dc4e8ed448fcc6066f
tranghasgit <tranghasgit@users.noreply.github.com>
2020-01-19 23:39:43 +0100

    update

--- js/main.js
+++ js/main.js
@@ -41,44 +41,6 @@ $(document).ready(function(){
     $('.communiItem').toggleClass('communiItemClicked');
   });
 
-//animate text footnote
-  $('.forTheFirstTime').textillate({
-    loop: true,
-    in: {
-  	// set the effect name
-    effect: 'wobble',
-
-    // set the delay factor applied to each consecutive character
-    delayScale: 1.5,
-
-    // set the delay between each character
-    delay: 50,
-
-    // set to true to animate all the characters at the same time
-    sync: true,
-
-    // randomize the character sequence
-    // (note that shuffle doesn't make sense with sync = true)
-    shuffle: false,
-
-    // reverse the character sequence
-    // (note that reverse doesn't make sense with sync = true)
-    reverse: false,
-
-    // callback that executes once the animation has finished
-    callback: function () {}
-  },
-    out: {
-    effect: 'swing',
-    delayScale: 2.5,
-    delay: 50,
-    sync: true,
-    shuffle: false,
-    reverse: false,
-    callback: function () {}
-  },
-  });
-
   $('.fnt_').textillate({
     loop: true,
     in: {
